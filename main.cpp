a203cb1c989a067c7a5914437f351fb6a2ff1e44
Christopher Blair <CBlair1986@gmail.com>
2009-02-08 22:45:09 -0500

    Rudimentary program running

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include <stdio>
-#include <stdlib>
+#include "libtcod.hpp"
 
 int main()
-{
-    // TODO: Make a room making device.
+{   
+    /*
+     * This will be a bit of a test. To see if it works, and such.
+     */
+    TCODConsole::initRoot(80,50,"CZGame",false);
+    bool endGame;
+    while ( ! endGame && ! TCODConsole::isWindowClosed() )
+    {
+        // Draw functions go here...
+        TCODConsole::flush();
+        TCOD_key_t key=TCODConsole::waitForKeypress(true);
+        // Updating functions go here...
+    }
+    // Cleanup goes here...
 }
